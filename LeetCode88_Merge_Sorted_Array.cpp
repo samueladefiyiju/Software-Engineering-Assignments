24fdd0c4bab6951e3c7fdfb162cdc23a252156ed
samueladefiyiju <56696040+samueladefiyiju@users.noreply.github.com>
2021-04-30 22:37:00 -0400

    Create LeetCode88_Merge_Sorted_Array.cpp

---
+++ LeetCode88_Merge_Sorted_Array.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+        m--;
+        n--; 
+        int index = nums1.size()-1;
+        while (index >= 0){
+            if (n<0){
+                nums1[index] = nums1[m--];
+                }
+            else if (m< 0){
+                nums1[index] = nums2[n--];
+            }
+            else{
+                if (nums1[m] > nums2[n]){
+                    nums1[index] = nums1[m--];
+                
+            }
+                else{
+                    nums1[index] = nums2[n--]; 
+                }
+               
+                    
+                    
+                }
+            index--; 
+                
+                
+                        
+            
+        }
+    }
+};
